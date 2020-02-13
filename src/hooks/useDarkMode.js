fe50f96a1c325dc5ec11699c1e629258085f3f12
Josue Rodriguez <jr2589213@gmail.com>
2020-02-13 14:46:37 -0800

    passed custom component to nav bar in order to allow darkmode to to work via toggle

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,17 +1,17 @@
-import useLocalStorage from './useLocalStorage';
+import { useLocalStorage } from './useLocalStorage';
 import { useEffect } from 'react'
 
-export const useDarkMode = () => {
-   const [darkMode, setDarkMode] = useLocalStorage('darkModeStatus')
+export const useDarkMode = (boolean) => {
+   const [darkMode, setDarkMode] = useLocalStorage('darkModeStatus', boolean)
     useEffect(() => {
         const bodyDom = document.querySelector('body')
 
-        if (darkMode) {
-        bodyDom.classList.add('dark-mode')
+        if (darkMode === true) {
+      return bodyDom.classList.add('dark-mode')
     } else {
-        bodyDom.classList.remove('dark-mode')
+       return bodyDom.classList.remove('dark-mode')
     }
 
-    return bodyDom
     }, [darkMode])
+    return [darkMode, setDarkMode]
 }
